c0f8835f1674ca26d0c38965daaa65210bd02fae
Marius13z <lemonlcsnet@yahoo.com>
2022-09-26 00:40:52 +0300

    test

--- index.js
+++ index.js
@@ -17,7 +17,8 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 // enable cross origin requests
 app.use(cors())
 
-app.use('https://gleeful-elf-0757dd.netlify.app', gifsRoutes)
+app.use('/', gifsRoutes)
+app.get('/', (req, res) => res.send('APP IS RUNNING'))
 
 // connect to MongoDB
 mongoose.connect(process.env.CONNECTION_URL, { useUnifiedTopology: true, useNewUrlParser: true })
